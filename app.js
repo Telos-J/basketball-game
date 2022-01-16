6946d3fa8d586c3541c46a67f23a2ab8f0d5b8c1
JustinLee1004 <75999975+JustinLee1004@users.noreply.github.com>
2022-01-17 01:04:09 +0900

    class, movement

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-console.log("hello")
+class Player  {
+    constructor(dom, x, y, speed) {
+        this.dom = document.querySelector(dom);
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.vx = 0;
+        this.vy = 0;
+    }
+   
+    
+    move() {
+        this.vx = 0
+        this.vy = 0
+        if (controller.arrowUp) this.vy = -this.speed
+        if (controller.arrowDown) this.vy = this.speed
+        if (controller.arrowLeft) this.vx = -this.speed
+        if (controller.arrowRight) this.vx = this.speed
+
+        this.x += this.vx
+        this.y += this.vy
+        this.dom.style.transform = `translate(${this.x}px, ${this.y}px)`
+    }
+}
+const purple = new Player('.purple', 400, 400, 3);
+
+function gameLoop() {
+    purple.move()
+    console.log(controller)
+    requestAnimationFrame(gameLoop)
+}
+const controller = {
+    arrowUp: false,
+    arrowDown: false,
+    arrowLeft: false,
+    arrowRight: false
+}
+addEventListener('keydown', e => {
+    if (e.code === 'ArrowUp') {
+       controller.arrowUp = true
+    } else if (e.code === 'ArrowLeft'){
+        controller.arrowLeft = true
+    } else if (e.code === 'ArrowDown'){
+        controller.arrowDown = true
+    } else if (e.code === 'ArrowRight'){
+        controller.arrowRight = true
+    } 
+})
+
+addEventListener('keyup', e => {
+    if (e.code === 'ArrowUp') {
+       controller.arrowUp = false
+    } else if (e.code === 'ArrowLeft'){
+        controller.arrowLeft = false
+    } else if (e.code === 'ArrowDown'){
+        controller.arrowDown = false
+    } else if (e.code === 'ArrowRight'){
+        controller.arrowRight = false
+    } 
+})
+
+
+
+gameLoop()
